5d0f51033bbce687fb58e9e86752dbedfef033e1
Prasetyo99 <prasetyoyoyo90@gmail.com>
2013-11-29 18:41:07 +0700

    Project1

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SiPRATAMA</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="superfish/src/css/superfish.css" media="screen">
+<script src="superfish/src/js/jquery.js"></script>
+<script src="superfish/src/js/hoverIntent.js"></script>
+<script src="superfish/src/js/superfish.js"></script>
+<script>
+
+		
+
+			$(document).ready(function(){
+
+				// initialise plugin
+				$('.nav').superfish();
+					//add options here if required
+				
+			});
+
+
+
+
+		</script>
+</head>
+
+<body>
+<div id="body_wrapper">
+	<div id="wrapper">
+		<div id="header">
+            <div id="site_title"><h1><a href="" rel="nofollow">-</a></h1></div>
+            <div id="menu">
+            <ul class="sf-menu nav" id="example">
+				<li class="current"><a href="index.php">Beranda</a></li>
+				<li>
+					<a href="#">Menu</a>
+					<ul>
+                		<li>
+							<a href="followed.html">Pertolongan Pertama kecelakaan</a>
+						</li>
+                    	<li>
+							<a href="followed.html">Pertolongan Pertama Bencana</a>
+						</li>
+						<li>
+                        	<a href="followed.html">Tips dan Saran</a>
+                        </li>
+                        <li>
+                        	<a href="#">Contact Us</a>
+                        </li>
+					</ul>
+				</li>
+				<li><a href="download.html">Unduh</a></li>
+            	<li><a href="pelacakan.html">Pelacakan</a></li>
+                <li><a href="Komentar.php" class="last">Komentar</a></li>
+			</ul>
+            </div> <!-- end of menu -->
+        </div><!-- end of header -->
+        
+        <div id="middle">
+          <div id="mid_left">
+                <div id="mid_title">Pertolongan Pertama</div>
+                <p>Tidak ada seorang pun yang dapat memperkirakan kapan kecelakaan atau serangan sakit mendadak akan terjadi.</p>
+                <a rel="nofollow" href="" class="viewall"></a>
+            </div>
+        <img src="images/first.png" /></div> <!-- end of middle -->
+
+    </div>
+</div>
+
+	<div class="rata" align="justify">
+	<div id="sipratama_main">
+    	<div class="cbox_fw">
+        	<div class="cbox_3b fp_box"><span class="b1"></span>
+            	<h4><a href="www.facebook.com">FACEBOOK</a></h4>
+                <p>GO AND LIKE US IN FACEBOOK</p>
+            </div>
+        	<div class="cbox_3b fp_box"><span class="b2"></span>
+            	<h4><a href="www.twitter.com">TWITTER</a></h4>
+                <p>FOLLOW US IN TWITTER </p>
+            </div>
+        	<div class="cbox_3b fp_box"><span class="b3"></span>
+            	<h4><a href="www.google.com">GOOGLE+</a></h4>
+                <p>FOLLOW US IN GOOGLE+</p>
+            </div>
+            <div class="cleaner"></div>
+        </div>
+        <div class="cbox_fw">
+        	<div class="cbox_large float_l">
+            	<h2>SELAMAT DATANG</h2>
+                <p>Kami menyediakan berbagai cara untuk melakukan pertolongan pertama baik dalam kecelakaan ataupun serangan sakit yang mendadak terjadi,dan juga kami menyediakan fitur pelacakan darurat agar mudah menemukan lokasi kecelakaan karena kecelakaan bisa menimpa siapa saja dan kapan saja.Kami berharap website kami dapat berguna bagi anda semua para pembaca</p>
+                <div class="cleaner h20"></div>	
+                <div class="cbox_small float_l">
+                	<h5 class="tw_bullet">Pertolongan Pertama Luka Gores</h5>
+                    <img src="images/lukagores.jpg" class="image_frame" />
+                    <p>Pernahkah anda mengalami luka karena teriris pisau? Atau luka karena tersayat benda tajam lainnya? Saya yakin tentu sebeagian besar orang pernah mengalaminya. Dan barangkali, tidak hanya satu atau dua kali terjadi dalam hidupnya. Lalu bagaimanakah anda merawat luka tersebut? Membiarkannya sembuh sendiri kah? Atau langsung membalutnya dengan plester luka? Benarkah anda yakin cara tersebut yang terbaik dalam merawat dan mengobati luka iris di kulit anda? Lantas bagaimanakah sebenarnya cara merawat dan mengobati luka iris yang benar?</p>
+                    <a href="#"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                 <div class="cbox_small float_r">
+                 	<h5 class="tw_bullet">Mendadak Tersedak</h5>
+                    <img src="images/tersedak.png" class="image_frame" />
+                    <p>Pernahkah anda tersedak ketika sedang makan? Ya, saya yakin setiap orang pernah merasakannya. Apa yang anda kemudian lakukan untuk meredakannya? Minum? Ya beberapa jawaban pasti menyebutkan minum. Tapi bagaimana sebetulnya yang harus kita lakukan ketika tersedak. Dan apa yang harus kita lakukan ketika semisal teman semeja kita yang tersedak?
+
+Tersedak terjadi ketika benda asing bersarang di pintu masuk tenggorokan, menutupi jalan nafas kita.  Pada dewasa, makanan menjadi penyebab tersering. Sedangkan pada anak anak biasanya benda – benda kecil yang potensial.</p>
+					<a href="#"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+            </div>
+            <div class="cbox_small float_r">
+            	<h2>Recent Post</h2>
+                	<div class="rp_box">
+                    	<img src="images/baby.png" /><p>Mana yang lebih berat? Diare pada anak-anak atau diare pada bayi?</p>
+                        <a href="#" class="more float_r"><span>&gt;&gt;</span> Read More</a>
+                        <div class="cleaner"></div>
+                    </div>
+                    <div class="rp_box">
+                    	<img src="images/lukagores.png" /><p>Pertolongan Pertama Luka Gores</p>
+                        <a href="#" class="more float_r"><span>&gt;&gt;</span> Read More</a>
+                    	<div class="cleaner"></div>
+                    </div>
+                    <div class="rp_box">
+                    	<img src="images/Obesity.png" /><p>Lebih Jauh tentang Obesitas</p>
+                        <a href="#" class="more float_r"><span>&gt;&gt;</span> Read More</a>
+                        <div class="cleaner"></div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
